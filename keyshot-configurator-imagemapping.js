a2af9e60d912eac37670ec58f7aee1ffd28fc7b2
dkjeong0815 <dkjeong0815@gmail.com>
2021-08-24 14:06:52 +0900

    iphone12max v1

---
+++ keyshot-configurator-imagemapping.js
@@ -0,0 +1,123 @@
+imagemappingdata =
+[
+    {
+        "image": "ip12max_WebXR.63.1.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+166"
+        ],
+        "studio": 1053
+    },
+    {
+        "image": "ip12max_WebXR.63.2.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+170"
+        ],
+        "studio": 1053
+    },
+    {
+        "image": "ip12max_WebXR.63.3.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+177"
+        ],
+        "studio": 1053
+    },
+    {
+        "image": "ip12max_WebXR.63.4.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+178"
+        ],
+        "studio": 1053
+    },
+    {
+        "image": "ip12max_WebXR.63.5.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+194"
+        ],
+        "studio": 1053
+    },
+    {
+        "image": "ip12max_WebXR.63.6.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+1267"
+        ],
+        "studio": 1053
+    },
+    {
+        "image": "ip12max_WebXR.63.7.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+166"
+        ],
+        "studio": 1054
+    },
+    {
+        "image": "ip12max_WebXR.63.8.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+170"
+        ],
+        "studio": 1054
+    },
+    {
+        "image": "ip12max_WebXR.63.9.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+177"
+        ],
+        "studio": 1054
+    },
+    {
+        "image": "ip12max_WebXR.63.10.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+178"
+        ],
+        "studio": 1054
+    },
+    {
+        "image": "ip12max_WebXR.63.11.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+194"
+        ],
+        "studio": 1054
+    },
+    {
+        "image": "ip12max_WebXR.63.12.png",
+        "modelsets": [
+            1244
+        ],
+        "multimaterials": [
+            "6+1267"
+        ],
+        "studio": 1054
+    }
+];
---
+++ keyshot-configurator-modeldata.js
@@ -0,0 +1,76 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "checked": true,
+                    "icon": "%eb%a1%9c%ec%a6%88%20%ea%b3%a8%eb%93%9c.png",
+                    "multimaterials": [
+                        "6+166"
+                    ],
+                    "name": "로즈 골드"
+                },
+                {
+                    "icon": "%ea%b7%b8%eb%9e%98%ed%8c%8c%ec%9d%b4%ed%8a%b8.png",
+                    "multimaterials": [
+                        "6+170"
+                    ],
+                    "name": "그래파이트"
+                },
+                {
+                    "icon": "%ec%8b%a4%eb%b2%84.png",
+                    "multimaterials": [
+                        "6+177"
+                    ],
+                    "name": "실버"
+                },
+                {
+                    "icon": "%ea%b3%a8%eb%93%9c.png",
+                    "multimaterials": [
+                        "6+178"
+                    ],
+                    "name": "골드"
+                },
+                {
+                    "icon": "%ed%8d%bc%ec%8b%9c%ed%94%bd%20%eb%b8%94%eb%a3%a8.png",
+                    "multimaterials": [
+                        "6+194"
+                    ],
+                    "name": "퍼시픽 블루"
+                },
+                {
+                    "icon": "%ed%8d%bc%ed%94%8c.png",
+                    "multimaterials": [
+                        "6+1267"
+                    ],
+                    "name": "퍼플"
+                }
+            ],
+            "name": "본체 재질"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "materialgroups": [
+                "본체 재질"
+            ],
+            "modelset": 1244,
+            "name": "Default"
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "%ec%a0%84%eb%a9%b4.png",
+            "id": 1053,
+            "name": "전면"
+        },
+        {
+            "icon": "%ed%9b%84%eb%a9%b4.png",
+            "id": 1054,
+            "name": "후면"
+        }
+    ]
+};
